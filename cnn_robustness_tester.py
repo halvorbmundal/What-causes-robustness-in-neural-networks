db8a4a0b3dedc3cad641db7c608bf702bc5ff7e8
Halvor Mundal <halvor.mundal@juniorconsulting.no>
2020-01-31 10:45:51 +0100

    move kernel size to be last parameter tested

--- cnn_robustness_tester.py
+++ cnn_robustness_tester.py
@@ -289,10 +289,10 @@ def main():
 
     make_result_file(CnnTestParameters.result_folder, CnnTestParameters.result_file)
     logging.basicConfig(filename='log.log', level="ERROR")
-    for filter_size in range(2, 128, 8):
-        for has_batch_normalization in [False]:
-            for depth in range(1, 10, 2):
-                for kernel_size in range(3, 15, 2):
+    for kernel_size in range(3, 15, 2):
+        for filter_size in range(2, 128, 8):
+            for has_batch_normalization in [False]:
+                for depth in range(1, 10, 2):
                     for activation_function_string in ["ada", "sigmoid", "arctan", "tanh"]:
 
                         parameters = CnnTestParameters()
