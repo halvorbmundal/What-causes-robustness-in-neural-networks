281f33e6f0d3b93fe7100204a272a6cf2a8d7a0d
Halvor Mundal <halvor.mundal@juniorconsulting.no>
2020-02-11 23:54:09 +0100

    Release keras resources

--- cnn_robustness_tester.py
+++ cnn_robustness_tester.py
@@ -212,6 +212,7 @@ def gpu_calculations(parameters):
     keras_lock.acquire()
     try:
         if not file_exists(parameters.file_name):
+            reset_keras()
             setDynamicGPUAllocation()
             print(f"\ntraining with {parameter_string(parameters)}\n", flush=True)
             train_nn(parameters.file_name,
@@ -223,9 +224,12 @@ def gpu_calculations(parameters):
                      parameters.use_padding_same,
                      parameters.use_early_stopping)
             print(f"\ndone training with {parameter_string(parameters)}\n", flush=True)
+            reset_keras()
 
         else:
             print("Neural network already created - {}".format(parameters.file_name), flush=True)
+    except:
+        print("error training network")
     finally:
         keras_lock.release()
 
@@ -256,6 +260,7 @@ def multithreadded_calculations(parameters):
         semaphore.acquire()
         try:
             print(f"\nCalculating robustness of {parameter_string(parameters)}\n", flush=True)
+            reset_keras()
             setDynamicGPUAllocation()
 
             if not file_exists(parameters.file_name):
@@ -366,34 +371,34 @@ def main():
             for filter_size in range(2, 64, 4):
                 for has_batch_normalization in [False]:
                     for depth in range(1, 5, 1):
-                            for use_early_stopping in [True, False]:
-                                for use_padding_same in [True]:
-                                    for use_cnnc_core in [True, False]:
+                        for use_early_stopping in [True, False]:
+                            for use_padding_same in [True]:
+                                for use_cnnc_core in [True, False]:
 
-                                        parameters = CnnTestParameters()
-                                        parameters.tf_activation = get_tf_activation_function_from_string(activation_function_string)
-                                        parameters.activation_function_string = activation_function_string
-                                        parameters.depth = depth
-                                        parameters.kernel_size = kernel_size
-                                        parameters.filter_size = filter_size
-                                        parameters.filters = [filter_size for i in range(depth)]
-                                        parameters.kernels = [kernel_size for i in range(depth)]
-                                        parameters.has_batch_normalization = has_batch_normalization
-                                        parameters.isDebugging = debugging
-                                        parameters.use_early_stopping = use_early_stopping
-                                        parameters.use_padding_same = use_padding_same
-                                        parameters.use_cnnc_core = use_cnnc_core
+                                    parameters = CnnTestParameters()
+                                    parameters.tf_activation = get_tf_activation_function_from_string(activation_function_string)
+                                    parameters.activation_function_string = activation_function_string
+                                    parameters.depth = depth
+                                    parameters.kernel_size = kernel_size
+                                    parameters.filter_size = filter_size
+                                    parameters.filters = [filter_size for i in range(depth)]
+                                    parameters.kernels = [kernel_size for i in range(depth)]
+                                    parameters.has_batch_normalization = has_batch_normalization
+                                    parameters.isDebugging = debugging
+                                    parameters.use_early_stopping = use_early_stopping
+                                    parameters.use_padding_same = use_padding_same
+                                    parameters.use_cnnc_core = use_cnnc_core
 
-                                        parameters.use_gpu = gpu
-                                        parameters.use_cpu = cpu
+                                    parameters.use_gpu = gpu
+                                    parameters.use_cpu = cpu
 
-                                        parameters.file_name = get_name_new_convention(parameters)
+                                    parameters.file_name = get_name_new_convention(parameters)
 
-                                        if debugging:
-                                            pool_init(l1, l2, sema)
-                                            multithreadded_calculations(parameters)
-                                        else:
-                                            pool.apply_async(multithreadded_calculations, (parameters,))
+                                    if debugging:
+                                        pool_init(l1, l2, sema)
+                                        multithreadded_calculations(parameters)
+                                    else:
+                                        pool.apply_async(multithreadded_calculations, (parameters,))
 
     pool.close()
     pool.join()
