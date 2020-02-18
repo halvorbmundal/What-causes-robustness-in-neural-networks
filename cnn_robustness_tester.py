08cf00c6dd5b9001ac69e78864ee25458e1b8c09
Halvor Mundal <halvor.mundal@juniorconsulting.no>
2020-02-18 17:59:25 +0100

    Bugfix?

--- cnn_robustness_tester.py
+++ cnn_robustness_tester.py
@@ -225,7 +225,7 @@ def train_nn(parameters, file_name, filters, kernels, epochs, tf_activation, bat
         reset_cuda()
         print(datetime.now())
         print(f"\ntraining with {parameter_string(parameters)}\n", flush=True)
-        with tf.Session():
+        with tf.Session().as_default():
             train_and_save_network(file_name,
                                    filters,
                                    kernels,
