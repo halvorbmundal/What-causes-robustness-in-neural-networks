58abf7d5b15c936fe9bb2212feacfd5607901bbb
Halvor Mundal <halvor.mundal@juniorconsulting.no>
2020-03-10 21:24:05 +0100

    .

--- cnn_robustness_tester.py
+++ cnn_robustness_tester.py
@@ -479,7 +479,7 @@ def main():
         for kernel_size in range(3, 8):
             for use_cnnc_core in [False]:
                 for filter_size in range(2, 64, 4):
-                    for has_batch_normalization in [False]:
+                    for has_batch_normalization in [True, False]:
                         for depth in range(1, 6, 1):
                             for use_early_stopping in [True]:
                                 for use_padding_same in [True]:
