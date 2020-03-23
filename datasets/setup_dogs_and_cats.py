add correct label. remove pickeling of sign language

datasets/setup_dogs_and_cats.py
@@ -34,7 +34,7 @@ def resize_images(path, imgs_path):
                             image = io.imread(f"{path}/{imgs_path}/{data_set}/{label}/{image_path}")
                             image = transform.resize(image, (64, 64))
                             data.append(image)
-                            labels.append(label)
+                            labels.append(index)
 
                     label_index += 1
 
@@ -87,6 +87,11 @@ class DogsAndCats():
         X_train, X_val, y_train, y_val = sklearn.model_selection.train_test_split(X_train, y_train, test_size=VAL_FRACTION,
                                                                                   random_state=1215, stratify=y_train)
 
+        num_classes = 2
+        y_test = np.eye(num_classes)[y_test]
+        y_val = np.eye(num_classes)[y_val]
+        y_train = np.eye(num_classes)[y_train]
+
         self.train_data = X_train
         self.train_labels = y_train
 
@@ -100,6 +105,7 @@ class DogsAndCats():
 
 if __name__ == "__main__":
     a = DogsAndCats()
+    print(a.train_labels.shape)
 
 
 

datasets/setup_sign_language.py
@@ -32,33 +32,16 @@ def convert_from_csv(path):
     data = np.array(data)
     labels = np.array(labels)
 
-    with open(f"{path}/np_x_data.pkl", 'wb') as f:
-        pickle.dump(data, f)
-
-    with open(f"{path}/np_y_data.pkl", 'wb') as f:
-        pickle.dump(labels, f)
-
     return data, labels
 
 def load_data(dataset_name):
     home = str(Path.home())
     path = f"{home}/numpy_datasets/{dataset_name}/"
-    img_path = "dataset"
 
-    if not os.path.exists(path + "/np_x_data.pkl"):
-        print("extracting from images")
-        X, Y = convert_from_csv(path)
-    else:
-        with open(path + "/np_x_data.pkl", 'rb') as f:
-            X = pickle.load(f)
-        with open(path + "/np_y_data.pkl", 'rb') as f:
-            Y = pickle.load(f)
+    X, Y = convert_from_csv(path)
 
     X = X.astype("float32") / 255.0
 
-    # one-hot encode the training and testing labels
-    #numLabels = len(np.unique(Y))
-    #Y = to_categorical(Y, numLabels)
     return X, Y
 
 class SignLanguage():
@@ -71,11 +54,16 @@ class SignLanguage():
 
         TEST_FRACTION = 0.1
         VAL_FRACTION = 0.1
+        num_classes = 25
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_train, y_train,
                                                                                     test_size=TEST_FRACTION,
                                                                                     random_state=1215, stratify=y_train)
         X_train, X_val, y_train, y_val = sklearn.model_selection.train_test_split(X_train, y_train, test_size=VAL_FRACTION,
                                                                                   random_state=1215, stratify=y_train)
+
+        y_test = np.eye(num_classes)[y_test]
+        y_val = np.eye(num_classes)[y_val]
+        y_train = np.eye(num_classes)[y_train]
 
         self.train_data = X_train
         self.train_labels = y_train
@@ -91,7 +79,8 @@ class SignLanguage():
         print("Done setting up sign language")
 
 if __name__ == "__main__":
-    a = DogsAndCats()
+    a = SignLanguage()
+    print(a.train_labels.shape)
 
 
 