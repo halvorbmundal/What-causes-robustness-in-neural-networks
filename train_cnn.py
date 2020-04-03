397b9f942b705c07e1c9f1061686d05ff40f4e7d
Halvor Mundal <halvor.mundal@juniorconsulting.no>
2020-04-03 15:55:46 +0200

    Remove multi gpu model

--- train_cnn.py
+++ train_cnn.py
@@ -76,10 +76,6 @@ def train(data, file_name, filters, kernels, num_epochs=50, batch_size=128, trai
 
         devices = get_available_gpus()
 
-        if len(devices) >= 2:
-            print(f"using {len(devices)} gpus")
-            model = tf.keras.utils.multi_gpu_model(model, gpus=len(devices))
-
         model.compile(loss=fn,
                       optimizer=optimizer,
                       metrics=['accuracy'])
