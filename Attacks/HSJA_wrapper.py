fcfd44ac19fc21f83bd115e1ac41461a4506c8b1
Halvor Mundal <halvor.mundal@juniorconsulting.no>
2020-04-30 16:24:36 +0200

    Add hop skip jump attack

---
+++ Attacks/HSJA_wrapper.py
@@ -0,0 +1,106 @@
+# Interface to run CW/EAD attacks
+
+import numpy as np
+from Attacks.HSJA import hsja
+from datasets.setup_cifar import CIFAR
+from datasets.setup_tinyimagenet import TinyImagenet
+from tensorflow.contrib.keras.api.keras.models import load_model
+from CNN_Cert.utils import generate_data
+import time as timer
+import random
+from CNN_Cert.train_resnet import *
+
+
+def loss(correct, predicted):
+    return tf.nn.softmax_cross_entropy_with_logits(labels=correct,
+                                                   logits=predicted)
+
+
+# Runs CW/EAD attack with specified norm
+def hsja_attack(file_name, norm, sess, num_image=10, data_set_class=MNIST(), targeted=False):
+    print("hsja attack", flush=True)
+    np.random.seed(1215)
+    tf.set_random_seed(1215)
+    random.seed(1215)
+
+    data = data_set_class
+
+    model = load_model(file_name,
+                       custom_objects={'fn': loss, 'tf': tf, 'ResidualStart': ResidualStart, 'ResidualStart2': ResidualStart2, 'tf': tf, 'atan': tf.math.atan})
+    inputs, targets, true_labels, true_ids, img_info = generate_data(data, samples=num_image, targeted=True, random_and_least_likely=True, target_type=0b0001,
+                                                                     predictor=model.predict, start=0)
+    if len(inputs) == 0:
+        return 0, 0
+
+    if targeted:
+        target_examples = get_target_examples(data, model, targets)
+        targets = np.argmax(targets, axis=1)
+    else:
+        target_examples = [None for i in range(len(targets))]
+        targets = [None for i in range(len(targets))]
+
+    if norm == "2":
+        constraint = "l2"
+        norm_fn = lambda x: np.sum(x ** 2, axis=(1, 2, 3))
+    elif norm == "i":
+        constraint = "linf"
+        norm_fn = lambda x: np.max(np.abs(x), axis=(1, 2, 3))
+    else:
+        raise ValueError("norm must be 2 or inf")
+
+    start_time = timer.time()
+    perturbed_input = attack_multiple(inputs, targets, target_examples, constraint, model)
+    UB = np.average(norm_fn(perturbed_input - inputs))
+    time_spent = (timer.time() - start_time) / len(inputs)
+    return UB, time_spent
+
+
+def get_target_examples(data, model, targets):
+    target_examples = []
+    for target in targets:
+        target_example_found = False
+        target_example_index = 0
+        while not target_example_found:
+            target_example_index = find_first(data.train_labels[target_example_index:], np.argmax(target))
+            target_example = data.train_data[target_example_index]
+            prediction = np.argmax(model.predict(np.array([target_example, ])))
+            if prediction == np.argmax(target):
+                target_example_found = True
+                target_examples.append(target_example)
+            target_example_index += 1
+    return target_examples
+
+
+def attack_multiple(imgs, targets, target_examples, constraint, model):
+    """
+    Perform the L_0 attack on the given images for the given targets.
+
+    If self.targeted is true, then the targets represents the target labels.
+    If self.targeted is false, then targets are the original class labels.
+    """
+
+    r = []
+    print(f"{len(imgs)} images and {len(targets)} targets")
+    for img, target, target_example in zip(imgs, targets, target_examples):
+        print(f"calclulating robustness for image {len(r) + 1}", flush=True)
+        r.append(hsja(model,
+                      img,
+                      clip_max=0.5,
+                      clip_min=-0.5,
+                      constraint=constraint,
+                      num_iterations=1000,
+                      gamma=1.0,
+                      target_label=target,
+                      target_image=target_example,
+                      stepsize_search='geometric_progression',
+                      max_num_evals=1e4,
+                      init_num_evals=100,
+                      verbose=True))
+    return np.array(r)
+
+
+def find_first(arr, label):
+    for i in range(len(arr)):
+        if np.argmax(arr[i]) == label:
+            return i
+    return -1
---
+++ upper_bound_HSJA.py
@@ -0,0 +1,115 @@
+import csv
+import gc
+import logging
+import os
+import sys
+from datetime import datetime
+
+import tensorflow as tf
+
+from Attacks.HSJA_wrapper import hsja_attack
+from cnn_robustness_tester import get_tf_activation_function_from_string, debugprint, file_exists, print_parameters, parameter_string, set_path, get_data, \
+    make_upper_bound_file, upper_bounds_csv_contains_file
+from hyper_parameters import hyper_parameters
+
+"""
+def add_l_norm_to_upper_bound_file(file_name="upper_bound.csv"):
+    csv_input = pd.read_csv(file_name)
+    if "l_norm" in csv_input.keys():
+        return
+    csv_input['l_norm'] = "i"
+    csv_input.to_csv(file_name, index=False)"""
+
+
+def write_to_upper_bound_file(parameters, upper_bound, time_elapsed, csv_file):
+    with open(csv_file, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(
+            [time_elapsed, upper_bound, parameters.file_name, parameters.l_norm])
+
+
+def upper_bound_calculations(parameters):
+    if parameters.l_norm == "1":
+        return
+
+    print(f"\nCalculating upper bound of {parameter_string(parameters)}\n", flush=True)
+
+    parameters.tf_activation = get_tf_activation_function_from_string(
+        parameters.activation_function_string, tf)
+
+    debugprint(parameters.isDebugging, "checking if model file exists")
+    if not file_exists(parameters.file_name, use_cache=False):
+        print("File does not exist {}".format(parameters.file_name), flush=True)
+        print_parameters(parameters)
+        logging.exception("\n =================\n\n"
+                          + str(datetime.now()) +
+                          "\nThe error was here: \n"
+                          + parameter_string(parameters) +
+                          "\n" + "File does not exist {}".format(parameters.file_name) +
+                          "\n\n")
+        return
+
+    csv_name = "HSJA_" + parameters.upper_bound_result_file
+    make_upper_bound_file(csv_name)
+
+    debugprint(parameters.isDebugging, "reading results csv")
+    if upper_bounds_csv_contains_file(csv_name, parameters.file_name, parameters):
+        print("Upper bounds already calculated for {}".format(parameters.file_name), flush=True)
+        print_parameters(parameters)
+        return
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+    config.log_device_placement = False
+    sess = tf.Session(config=config)
+
+    with sess.as_default():
+        upper_bound, time_spent = hsja_attack(parameters.file_name,
+                                              parameters.l_norm,
+                                              sess,
+                                              parameters.num_image,
+                                              dataset_data)
+    if time_spent == 0:
+        return
+
+    debugprint(parameters.isDebugging, "writing upper bound to file")
+    write_to_upper_bound_file(parameters, upper_bound, time_spent, csv_name)
+
+    print("wrote upper bound to file", flush=True)
+    print_parameters(parameters)
+
+    gc.collect()
+    return
+
+
+def init_globals(data):
+    global dataset_data
+    dataset_data = data
+
+
+def main():
+    _, path, dataset = sys.argv
+    set_path(path)
+    set_path(dataset)
+    print(path, dataset, flush=True)
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    try:
+        model_files = os.listdir("output/models")
+    except:
+        model_files = []
+
+    data = get_data(dataset)
+    init_globals(data)
+
+    params = hyper_parameters(dataset=dataset, model_files=model_files)
+
+    for parameters in params:
+        upper_bound_calculations(parameters)
+
+        gc.collect()
+
+
+if __name__ == "__main__":
+    main()
