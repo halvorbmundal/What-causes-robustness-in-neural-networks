74a1a0b7448a4e6447c417a4a9520ffad97e1cfe
Halvor Mundal <halvor.mundal@juniorconsulting.no>
2020-05-05 10:48:06 +0200

    Less hyperparameters

--- hyper_parameters.py
+++ hyper_parameters.py
@@ -5,6 +5,7 @@ from enum import Enum
 class NnArchitecture(Enum):
     ONLY_CNN = "only_cnn"
 
+
 class CnnTestParameters:
     dataset = "mnist"
     nn_architecture = NnArchitecture.ONLY_CNN.value
@@ -23,11 +24,13 @@ class CnnTestParameters:
     result_file = 'results.csv'
     upper_bound_result_file = 'upper_bound.csv'
 
+
 def boolToString(b):
     if b:
         return "T"
     else:
         return "F"
+
 
 def get_name_new_convention(parameter_class):
     directory = "output/models"
@@ -44,12 +47,13 @@ def get_name_new_convention(parameter_class):
                 boolToString(parameter_class.use_early_stopping),
                 boolToString(parameter_class.use_padding_same))
 
+
 def hyper_parameters(dataset,
                      model_files,
                      debugging=False,
                      gpu=False,
                      cpu=False):
-    filter_size_range = range(8, 81, 8)
+    filter_size_range = range(8, 73, 16)
     depth_range = range(1, 6, 1)
     kernel_size_range = range(3, 8, 1)
     cnnc_choices = [False]
@@ -63,27 +67,27 @@ def hyper_parameters(dataset,
                     for use_padding_same in [False]:
                         for use_early_stopping in [True]:
                             for has_batch_normalization in bn_choices:
-                                    for depth in depth_range:
-                                        for filter_size in filter_size_range:
-                                            parameters = CnnTestParameters()
-                                            parameters.activation_function_string = activation_function_string
-                                            parameters.depth = depth
-                                            parameters.kernel_size = kernel_size
-                                            parameters.filter_size = filter_size
-                                            parameters.filters = [filter_size for i in range(depth)]
-                                            parameters.kernels = [kernel_size for i in range(depth)]
-                                            parameters.has_batch_normalization = has_batch_normalization
-                                            parameters.isDebugging = debugging
-                                            parameters.use_early_stopping = use_early_stopping
-                                            parameters.use_padding_same = use_padding_same
-                                            parameters.use_cnnc_core = use_cnnc_core
-                                            parameters.dataset = dataset
-                                            parameters.model_files = model_files
-                                            parameters.l_norm = l_norm
+                                for depth in depth_range:
+                                    for filter_size in filter_size_range:
+                                        parameters = CnnTestParameters()
+                                        parameters.activation_function_string = activation_function_string
+                                        parameters.depth = depth
+                                        parameters.kernel_size = kernel_size
+                                        parameters.filter_size = filter_size
+                                        parameters.filters = [filter_size for i in range(depth)]
+                                        parameters.kernels = [kernel_size for i in range(depth)]
+                                        parameters.has_batch_normalization = has_batch_normalization
+                                        parameters.isDebugging = debugging
+                                        parameters.use_early_stopping = use_early_stopping
+                                        parameters.use_padding_same = use_padding_same
+                                        parameters.use_cnnc_core = use_cnnc_core
+                                        parameters.dataset = dataset
+                                        parameters.model_files = model_files
+                                        parameters.l_norm = l_norm
 
-                                            parameters.use_gpu = gpu
-                                            parameters.use_cpu = cpu
+                                        parameters.use_gpu = gpu
+                                        parameters.use_cpu = cpu
 
-                                            parameters.file_name = get_name_new_convention(parameters)
-                                            parameter_list.append(parameters)
+                                        parameters.file_name = get_name_new_convention(parameters)
+                                        parameter_list.append(parameters)
     return parameter_list
